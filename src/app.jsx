c01d06d5b6963098939400223fae4b549d1cb003
Sam Walsh <samjwalsh05@gmail.com>
2023-08-16 09:37:42 +0100

    Created hamburger menu frame, changed beep

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Order from "./components/Order.jsx";
 import OrderBar from "./components/OrderBar.jsx";
 import MenuBar from "./components/MenuBar.jsx";
 import Menu from "./components/Menu.jsx";
+import HamburgerMenu from "./components/HamburgerMenu.jsx";
 
 const domNode = document.getElementById("App");
 const root = ReactDOM.createRoot(domNode);
@@ -14,9 +15,11 @@ function App() {
   const [menuState, setMenuState] = useState("");
   const [currentOrder, setCurrentOrder] = useState("");
   const [order, setOrder] = useState([]);
+  const [hamburger, sethamburger] = useState(false);
 
   return (
     <div className="container" id="Container">
+      <HamburgerMenu hamburger={hamburger} sethamburger={sethamburger} />
       <MenuBar menuState={menuState} />
       <OrderBar />
       <Menu
