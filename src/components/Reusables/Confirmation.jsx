d7b19cd8baed4007f0a9264d453eeb0d46773159
Sam Walsh <samjwalsh05@gmail.com>
2023-10-16 18:59:56 +0100

    remove box around settings

---
+++ src/components/Reusables/Confirmation.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import log from "../../tools/logging";
+import playBeep from "../../tools/playBeep";
+
+export default function Confirmation(props) {
+    return (<div className="confirmationContainer"></div>)
+} 
+
