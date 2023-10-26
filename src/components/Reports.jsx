ec5ed46d7856216f878557f915bfd4b7e4a31eca
Sam Walsh <samjwalsh05@gmail.com>
2023-10-26 23:31:11 +0100

    hmm

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -84,7 +84,7 @@ export default function Reports(props) {
         return (
           <div
             key={order.time}
-            className='orderbox border border-colour rounded flex  flex-col '>
+            className='orderbox border border-colour rounded flex max-h-96 flex-col '>
             <div className='flex flex-row w-full p-2 justify-between border-b border-colour blue rounded-t'>
               <div className='text-2xl self-end'>
                 Order No. {orders.length - index}
