0b83a601df9bf4a2330551be7c180bf4ba85ab69
Sam Walsh <samjwalsh05@gmail.com>
2023-10-26 23:15:06 +0100

    actually fixed

--- src/components/TitleBar/HamburgerMenu.jsx
+++ src/components/TitleBar/HamburgerMenu.jsx
@@ -49,9 +49,9 @@ export default function HamburgerMenu(props) {
       <TitleBar setHamburger={setHamburger} />
       <Keypad />
       <Dialog />
-      <div className='fixed top-0 grid grid-cols-12 grid-rows-1 w-screen h-screen'>
-        <div className='row-span-1 col-span-3 flex backgroundcolour flex-col border-colour border-r'>
-          <div className='flex flex-row  w-100 justify-between p-2 border-b border-colour text-left items-stretch z-10'>
+      <div className='fixed top-0 grid grid-cols-12 grid-rows-1 w-screen h-screen z-50'>
+        <div className='row-span-1 col-span-3 flex backgroundcolour flex-col border-colour border-r '>
+          <div className='flex flex-row  w-100 justify-between p-2 border-b border-colour text-left items-stretch '>
             <div className='text-2xl self-end'>Menu</div>
             <div
               className='justify-end btn--minus btn shadow w-8 h-8 cnter-items'
