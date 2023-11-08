71ce7747ff9c5f465ddfa80e3436863b431acdfc
Sam Walsh <samjwalsh05@gmail.com>
2023-11-08 19:32:40 +0000

    hmm

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -12,10 +12,10 @@ export default function TitleBar(props) {
   return (
     <div className='flex flex-row justify-between border-b border-colour h-10'>
       <div
-        className='p-1 border-r border-colour black'
+        className='p-1 border-r border-colour black h-full w-12 cnter-items'
         onContextMenu={(e) => handleClickHamburger(setHamburger)}
         onTouchStart={(e) => handleClickHamburger(setHamburger)}>
-        <img src={hamburger} className='w-8 invert-icon cnter-items h-full' />
+        <img src={hamburger} className='w-10 invert-icon cnter-items ' />
       </div>
       <div className='flex flex-row items-center justify-end w-full font-mono'>
         <div className='border-l border-colour h-full cnter-items px-1'>
